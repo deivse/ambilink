f33fd7bd5560f20ec55375c0d23ce1481145c0f8
desiaiva <desiaiva@fit.cvut.cz>
2023-03-21 12:30:06 +0100

    Make dependency installation script more robust.

--- code/vst/install_dependencies.sh
+++ code/vst/install_dependencies.sh
@@ -8,10 +8,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 ensure_installed() {
-    dpkg -l | grep -q "[[:space:]]$1"
+    dpkg -l | grep -q "[[:space:]]$1[[:space:]]"
     if [ $? -ne 0 ]; then
         echo "The package $1 will now be installed. You may be prompted for your password."
-        sudo apt-get install $1
+        sudo apt-get -y install $1
     fi;
 }
 
@@ -20,6 +20,9 @@ echo "======== Installing package dependencies ========"
 echo "================================================="; echo
 
 ensure_installed bear # to generate compile_commands.json from Makefile build
+ensure_installed python3
+ensure_installed python3-pip
+ensure_installed cmake
 
 ### JUCE dependencies: ###
 ensure_installed pkg-config 
@@ -39,7 +42,14 @@ echo; echo "================================================="
 echo "=========== Installing conan packages ==========="
 echo "================================================="; echo
 
-conan install . -if build --profile=./conan_profiles/ubuntu20.conanprofile --build=missing
+if pip3 list 2>/dev/null | grep -q '^conan[[:space:]]*2'; then
+    echo "ERROR: conan 2.X installation detected. Only conan 1.X is currently supported.";
+    exit 1;
+fi;
+
+pip3 install "conan<2.0"
+conan=$(python3 -m site --user-base)/bin/conan
+$conan install . -if build --profile=./conan_profiles/ubuntu20.conanprofile --build=missing
 
 echo; echo "================================================="
 echo "============= Building dependencies ============="
